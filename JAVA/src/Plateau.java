30e24318775d2f54bc3647ad7c7b150c60e27c7a
LFSysdom <louis-francois.garnier@isep.fr>
2018-12-03 15:29:33 +0100

    tt

--- JAVA/src/Plateau.java
+++ JAVA/src/Plateau.java
@@ -1,9 +1,43 @@
+//package jeu;
 
-public class Plateau {
+import java.util.*;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public class Plateau {
+		
+		public static void main(String[] args) {
+			Plateau p1 = new Plateau(8,5);
+			p1.afficher();
+			}
 
+		private int nbrligne;
+		private int nbrcolonne;
+		private char [][] plateau;
+	
+		//Constructeur
+		public Plateau(int n, int b) {
+			nbrligne = n;
+			nbrcolonne = b;
+			plateau = new char [nbrligne][nbrcolonne];
+			
+			for(int i=0; i<nbrligne; i++) {
+				for(int j=0; j<nbrcolonne; j++) {
+					
+					plateau[i][j] = 'x';
+				}
+			}
+		}
+		
+		//Ascenseurs
+		//Methodes
+		public void afficher() {
+			System.out.println();
+			for(int i= 0; i<nbrligne; i++) {
+				for(int j=0; j<nbrcolonne; j++) {
+					System.out.print(" | " +plateau[i][j]);
+				}
+				System.out.println(" | ");
+			}
+			System.out.println();
+		}	
 	}
-
-}
+	
