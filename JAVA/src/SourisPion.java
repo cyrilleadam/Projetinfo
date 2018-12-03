a58f99fa6c4a34db272d260cc56b220cdf2c88db
Adam <adam.cyrille@hotmail.fr>
2018-12-03 16:44:54 +0100

    seance

--- JAVA/src/SourisPion.java
+++ JAVA/src/SourisPion.java
@@ -2,9 +2,32 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 public class SourisPion implements MouseListener {
+	private Plateau plateau;
+	private Roi roi;
 	
 
 
-	}
+public SourisPion(Roi roi, Plateau plateau){
+	this.plateau=plateau;
+	this.roi=roi;
+}
 
+public void mouseClicked(MouseEvent arg0) {
+	
+}
 
+public void mouseEntered(MouseEvent arg0) {
+
+}
+
+public void mouseExited(MouseEvent arg0) {
+	
+}
+
+public void mousePressed(MouseEvent arg0) {
+	plateau.PlacerRoi(roi);
+}
+
+public void mouseReleased(MouseEvent arg0) {	
+}
+}
