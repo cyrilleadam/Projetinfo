c74c505c144d690569ce8dd1518004cb783407e8
Adam <adam.cyrille@hotmail.fr>
2018-12-18 13:34:04 +0100

    java

--- JAVA/src/Royaume.java
+++ JAVA/src/Royaume.java
@@ -1,9 +1,25 @@
-
 public class Royaume {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int TAILLE;
+	public Domino[][] dominos;
+	public Domino dominoinit;
+	
+	
+public Royaume (int TAILLE) {
+	this.TAILLE=TAILLE;
+	dominos=new Domino[TAILLE]TAILLE];
+	for (int i=0; i<TAILLE;i++) {
+		for (int j=0; j<TAILLE;j++) {
+			ajouterDomino(new Domino(null,0),i,j);
+		
+		}
 	}
+	dominos[x][y]=new Doino(x,0);
+	
+}
 
+
+private void ajouterDomino(Domino domino, int i, int j) {
+	// TODO Auto-generated method stub
+	
+}
 }
