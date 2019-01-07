7aa5ff36c625e8cd63803abd3d450b5d476a67f6
Adam <adam.cyrille@hotmail.fr>
2019-01-07 09:05:20 +0100

    php

---
+++ JAVA/src/Royaume.java
@@ -0,0 +1,25 @@
+public class Royaume {
+	public int TAILLE;
+	public Domino[][] dominos;
+	public Domino dominoinit;
+	
+	
+public Royaume (int TAILLE) {
+	this.TAILLE=TAILLE;
+	dominos=new Domino[TAILLE]TAILLE];
+	for (int i=0; i<TAILLE;i++) {
+		for (int j=0; j<TAILLE;j++) {
+			ajouterDomino(new Domino(null,0),i,j);
+		
+		}
+	}
+	dominos[x][y]=new Doino(x,0);
+	
+}
+
+
+private void ajouterDomino(Domino domino, int i, int j) {
+	// TODO Auto-generated method stub
+	
+}
+}
